75ba1fd4ef3cf5edd55b41c7e6b4d7382da1f278
obarboza <obarboza@pe.ibm.com>
2021-03-08 21:28:27 -0500

    crawl spider

---
+++ worldometers/spiders/best_movies.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+
+
+class BestMoviesSpider(CrawlSpider):
+    name = 'best_movies'
+    allowed_domains = ['imdb.com']
+    start_urls = ['https://www.imdb.com/search/title/?groups=top_250&sort=user_rating,desc&view=simple']
+
+    rules = (
+        Rule(LinkExtractor(restrict_xpaths="//span[@class='lister-item-header']/span[@title]/a"), callback='parse_item', follow=True),
+    )
+
+    def parse_item(self, response):
+        # item = {}
+        #item['domain_id'] = response.xpath('//input[@id="sid"]/@value').get()
+        #item['name'] = response.xpath('//div[@id="name"]').get()
+        #item['description'] = response.xpath('//div[@id="description"]').get()
+        # return item
+        # print(response.url)
+        yield {
+            'title': response.xpath("//div[@class='title_wrapper']/h1/text()").get(),
+            'year': response.xpath("//span[@id='titleYear']/a/text()").get(),
+            'duration': response.xpath("normalize-space(//div[@class='title_wrapper']//time[@datetime]/text())").get(),
+            'genre': response.xpath("(//div[@class='subtext']/a)[1]/text()").get(),
+            'rating': response.xpath("//span[@itemprop='ratingValue']/text()").get(),
+            'movie_url': response.xpath("(//a[@data-video])[1]/@href").get()
+        }
